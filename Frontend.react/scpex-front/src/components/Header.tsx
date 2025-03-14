377e78052a422d797c120324355ec753f694d381
Feiron <53085628+fr1g@users.noreply.github.com>
2025-03-15 01:23:09 +0300

    不破不立

--- Frontend.react/scpex-front/src/components/Header.tsx
+++ Frontend.react/scpex-front/src/components/Header.tsx
@@ -22,32 +22,49 @@ import { useLocation } from "react-router";
 
 
 export default function Header({ credential = null } : { credential: UserCredential | null }){
+    
+    const [darkMode, setDarkMode] = useState(true);
 
-    let location = useLocation();
-
-    const themeDetector = useThemeDetector();
-    const [isDarkMode, setDarkMode] = useState(themeDetector);
-
-    useEffect(() => {    
-        setDarkMode(!themeDetector);
-        localStorage.theme = isDarkMode ? "dark" : "light";   
-        if (isDarkMode) {       
-            document.documentElement.setAttribute("class", "dark");
-            return;
-        }
-        document.documentElement.setAttribute("class", "");  
-    }, []);
-
-    function switchTheme() {
-        setDarkMode(!isDarkMode);
-
-        localStorage.theme = isDarkMode ? "dark" : "light";
-        if (isDarkMode) {
-            document.documentElement.setAttribute("class", "dark");
-            return;
-        }
-        document.documentElement.setAttribute("class", "");
+    function switchTheme (){
+        setTheme(!darkMode);
     }
+
+    function setTheme (setAsDark: boolean){
+        if(setAsDark){
+            document.documentElement.setAttribute("class", "dark");
+            localStorage.theme = "dark";
+            setDarkMode(true);
+        }else{
+            document.documentElement.setAttribute("class", "");
+            localStorage.theme = "light";
+            setDarkMode(false);
+        }
+
+    }    
+
+    useEffect(() => {
+        if(localStorage.theme == undefined || localStorage.theme == null){
+        // if no value then use browser settings
+            setTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
+        }else{
+        // if got value then apply the value
+            setTheme(localStorage.theme == "dark");
+        }
+        // set up matches and update when preference changed
+        window.matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener("change", (e: MediaQueryListEvent) => {
+            
+                console.log(`detected environment theme changed: dark`)
+                setTheme(e.matches);
+        });
+
+        // window.matchMedia("(prefers-color-scheme: light)")
+        //     .addEventListener("change", (e: MediaQueryListEvent) => {
+            
+        //         console.log(`detected environment theme changed: light`)
+        //         setTheme(!e.matches);
+        // });
+    }, []);
 
     return <>
         <div className="fixed w-full p-2 bg-slate-600 flex flex-shrink px-5 gap-5 shadow-lg header z-very-top" >
@@ -72,7 +89,7 @@ export default function Header({ credential = null } : { credential: UserCredent
         <div className="flex-grow"></div>
         <div className="text-lg ?font-semibold mb-1 mr-1 text-white ">
             <Button  borderless paddingless onClick={switchTheme} >  
-                <Icon fix pua={ document.documentElement.classList.contains("dark") ? 'e706' : 'e708'} />
+                <Icon fix pua={ darkMode ? 'e706' : 'e708'} />
             </Button>
         </div>
         <Button  className="text-white bg-transparent hover:bg-blue-500/70 active:bg-blue-600/50 transition ">
