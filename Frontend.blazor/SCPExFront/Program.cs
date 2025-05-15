623c3578034a1b745e88cab6d2101b055747b228
Feiron <53085628+fr1g@users.noreply.github.com>
2025-05-16 02:27:38 +0300

    Update Program.cs

--- Frontend.blazor/SCPExFront/Program.cs
+++ Frontend.blazor/SCPExFront/Program.cs
@@ -8,4 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var heyhey = """
+                  OKAY now you should know that I need C# displayed on the place inside the Github Detail page of this repo.
+                  So im adding useless lines of C# in this file, which should be a critical part of once-imagined blazor frontend.
+                  fvck it java. LONG LIVE THE C SHARP!
+  
+             """;
+
+Console.WriteLine(heyhey);
+
 await builder.Build().RunAsync();
